e014527418eda162b793d5cc240653bcb3fc4e3a
TariqRasheed12 <77362462+Tariq-Rasheed1212@users.noreply.github.com>
2021-06-16 20:21:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Declare and initialize the variables for exercise 1 here:
+let engineIndicatorLight = "green";
+    engineIndicatorLight = "black";
+    engineIndicatorLight = "green blinking";
+
+
+// BEFORE running the code, predict what will be printed to the console by the following statements:
+
+if (engineIndicatorLight === "green") {
+   console.log("engines have started");
+} else if (engineIndicatorLight === "green blinking") {
+   console.log("engines are preparing to start");
+} else {
+   console.log("engines are off");
+}

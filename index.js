c4a4cfc72ddd7273551f48efe34bdd41a4f772ed
TariqRasheed12 <77362462+Tariq-Rasheed1212@users.noreply.github.com>
2021-06-16 20:24:15 +0000

    declared engineIndicatorLight as green.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Declare and initialize the variables for exercise 1 here:
 let engineIndicatorLight = "green";
-    engineIndicatorLight = "black";
-    engineIndicatorLight = "green blinking";
 
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
